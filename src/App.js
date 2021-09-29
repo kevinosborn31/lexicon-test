10f3515f73205b3025b3ebd67ea53837eed60266
Kevin <kevinosborn31@gmail.com>
2021-09-30 07:09:54 +1000

    API call fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/style.css';
-import { useEffect, useState } from 'react';
 import Movies from './components/Movies';
 
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,10 @@
 import MovieListing from './MovieListing';
+import { useState, useEffect } from 'react';
 
 function Movies() {
 
-    const [state, setState] = useState([])
+    const [state, setState] = useState()
+
     const url = `https://challenge.lexicondigital.com.au/api/v2/filmworld/movies`
     useEffect(() => {
       fetch(url, {
@@ -11,13 +13,16 @@ function Movies() {
             "x-api-key": "Yr2636E6BTD3UCdleMkf7UEdqKnd9n361TQL9An7"
         }
     })
-    .then(
-            res => setState(res.data),
-        )
-  })
+    .then(response => response.json())
+    .then(data => setState(data.Movies));
+  }, []);   
   
   console.log(state);
-  
+
+  return (
+    <MovieListing />
+  );
+
 }
 
 export default Movies;
