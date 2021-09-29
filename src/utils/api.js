762c96132991530574ac128a422c239bf5fb840c
Kevin <kevinosborn31@gmail.com>
2021-09-30 07:49:14 +1000

    API refactor

---
+++ src/utils/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+  url: 'https://challenge.lexicondigital.com.au/api/v2/',
+  headers: {
+      'x-api-key': 'Yr2636E6BTD3UCdleMkf7UEdqKnd9n361TQL9An7'
+    }
+})
+
+export default api;
