e120fe2f7090dbbc59326b5bcdb7ca215fd11252
Kevin <kevinosborn31@gmail.com>
2021-09-30 14:26:53 +1000

    Pull data through to movie container

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -4,10 +4,16 @@ const MoviesContainer = ({movieList}) => {
 
   return(
     <div>
+    {movieList.movies.map(({id, poster, title, cinemaPrice, filmPrice}) => (
           <MovieListing 
-
+            id={id} 
+            image={poster} 
+            title={title} 
+            key={`${title}-${id}`}
+            cinemaPrice={cinemaPrice} 
+            filmPrice={filmPrice}  
           />
-
+    ))}
     </div>
   )
 }
